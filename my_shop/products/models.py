276bb463a5c1d80d58ad83f577669a6df3aefecd
AnatoliySt20 <strikun.a.p@gmail.com>
2017-07-25 20:56:09 +0300

    templates

--- my_shop/products/models.py
+++ my_shop/products/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places =2, default=0)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
     def __str__(self):
         return "%s, %s" % (self.price, self.name)
 
